852fe68ce8ccafdb69e1f37b918e7327d35ef7d9
Georg Federmann <georg.federmann@gmail.com>
2016-07-14 16:08:27 +0200

    Initial commit

---
+++ src/main/java/org/poormanscastle/products/hit2assext/DocDesignExtensions.java
@@ -0,0 +1,12 @@
+package org.poormanscastle.products.hit2assext;
+
+/**
+ * Created by georg on 7/13/16.
+ */
+public final class DocDesignExtensions {
+
+    public static String helloWorld() {
+        return "Hello, World!";
+    }
+
+}
